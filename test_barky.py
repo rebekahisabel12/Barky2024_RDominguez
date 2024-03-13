2314ef928cf45a3a45e00e80e53cf4c90b68b70a
Rebekah Dominguez <ridominguez2@buffs.wtamu.edu>
2024-03-13 14:27:47 -0500

    Added tes_barky and made a django project

---
+++ test_barky.py
@@ -0,0 +1,1 @@
+import unittest from barky
--- test_commands.py
+++ test_commands.py
@@ -5,8 +5,11 @@
 # we will use pytest: https://docs.pytest.org/en/stable/index.html
 
 # should we test quit? No, its behavior is self-evident and not logic dependent
+
+
 def test_quit_command():
     pass
+
 
 # okay, should I test the other commands?
 # not really, they are tighly coupled with sqlite3 and its use in the database.py module
